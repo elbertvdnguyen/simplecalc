dda228da9de0282181bfa1e6fde7c3a7f1632a5a
Figna002 <figna002@ucr.edu>
2020-06-03 12:00:59 -0700

    Finished basic calc files and operations

---
+++ calc.cpp
@@ -0,0 +1,120 @@
+#include "calc.hpp"
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <math.h>
+
+class Calc{
+        private:
+	double valOne = 0;
+	double valTwo = 0;
+        double saveAns = 0;
+	string op = "";
+	string promptCalc = "Enter an equation (Use an operator to use a previous answer.) (q to quit): ";
+	string invalidInput = "Input is not a number or operator.";
+        public:
+        void add(double numOne, double numTwo){
+		valOne = numOne;
+		valTwo = numTwo;
+		op = "+";
+		saveAns = valOne + valTwo;
+		print();
+		return;	
+	}
+        void add(double numTwo){
+		valOne = saveAns;
+		valTwo = numTwo;
+		op = "+";
+		saveAns = valOne+valTwo;
+		print();
+		return;
+	}
+        void sub(double numOne, double numTwo){
+		valOne = numOne;
+                valTwo = numTwo;
+                op = "-";
+                saveAns = valOne - valTwo;
+                print();
+                return;
+	}
+        void sub(double numTwo){
+		valOne = saveAns;
+		valTwo = numTwo;
+		op = "-";
+		saveAns = valOne - valTwo;
+		print();
+		return;
+	}
+        void mult(double numOne, double numTwo){
+		valOne = numOne;
+                valTwo = numTwo;
+                op = "*";
+                saveAns = valOne * valTwo;
+                print();
+                return;
+	}
+        void mult(double numTwo){
+		valOne = saveAns;
+                valTwo = numTwo;
+                op = "*";
+                saveAns = valOne * valTwo;
+                print();
+                return;
+	}
+        void div(double numOne, double numTwo){
+		valOne = numOne;
+                valTwo = numTwo;
+                op = "/";
+                saveAns = valOne / valTwo;
+                print();
+                return;
+	}
+        void div(double numTwo){
+		valOne = saveAns;
+                valTwo = numTwo;
+                op = "/";
+                saveAns = valOne / valTwo;
+                print();
+                return;
+	}
+        void pow(double numOne, double numTwo){
+		valOne = numOne;
+                valTwo = numTwo;
+                op = "**";
+                saveAns = pow(valOne, valTwo);
+                print();
+                return;
+
+	}
+        void pow(double numTwo){
+		valOne = saveAns;
+                valTwo = numTwo;
+                op = "**";
+                saveAns = pow(valOne, valTwo);
+                print();
+                return;
+
+	}
+        void mod(double, double){
+		valOne = numOne;
+                valTwo = numTwo;
+                op = '%';
+                saveAns = valOne % valTwo;
+                print();
+                return;
+	}
+        void mod(double){
+		valOne = saveAns;
+                valTwo = numTwo;
+                op = '%';
+                saveAns = valOne % valTwo;
+                print();
+                return;
+	}
+        void evaluate();
+        void getOp();
+	void display();
+        void print(){
+		cout << valOne << op << valTwo << saveAns << endl;
+	}
+}
---
+++ calc.hpp
@@ -0,0 +1,32 @@
+#ifndef CALC_HPP
+#define CALC_HPP
+
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+class Calc{
+	private:
+	double valOne = 0;
+	double ValTwo = 0;
+	double saveAns = 0;
+	public:
+	double add(double, double);
+	double add(double);
+	double sub(double, double);
+	double sub(double);
+	double mult(double, double);
+	double mult(double);
+	double div(double, double);
+	double div(double);
+	double pow(double, double);
+	double pow(double);
+	double mod(double, double);
+	double mod(double);
+	void evaluate();
+	void getOp();
+	void display();		
+}
+#endif
