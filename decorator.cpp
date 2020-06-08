e87f827a109de85545f18b01353a6c4a83666098
Awesomeleek349 <klee229@ucr.edu>
2020-06-08 10:36:36 -0700

    decorator files

---
+++ decorator.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+#include <cmath>
+#include <math.h>
+
+#include "decorator.hpp"
+#include "calc.hpp"
+
+Decorator::Decorator(Calc* calc){
+
+	this->calc = calc;
+
+}
+
+
+Calc* Decorator::get_calc(){
+
+	return this->calc;
+
+}
+
+void Decorator::set_calc(Calc* calc){
+
+	this->calc = calc;
+
+}
+
+ConvertToDegrees::ConvertToDegrees(Calc* calc) : Decorator(calc){};
+double ConvertToDegrees::evaluate(){
+	return (get_calc()->evaluate() * 180)/M_PI;
+
+}
+
+ConvertToRadians::ConvertToRadians(Calc* calc) : Decorator(calc){};
+double ConvertToRadians::evaluate(){
+	return (get_calc()->evaluate() * M_PI)/180;
+
+}
+
+SineCalculation::SineCalculation(Calc* calc) : Decorator(calc){};
+double SineCalculation::evaluate(){
+	return sin(get_calc()->evaluate());	
+
+}
+
+CosineCalculation::CosineCalculation(Calc* calc) : Decorator(calc){};
+double CosineCalculation::evaluate(){
+	return cos(get_calc()->evaluate());
+
+}
+
+TangentCalculation::TangentCalculation(Calc* calc) : Decorator(calc){};
+double TangentCalculation::evaluate(){
+	return tan(get_calc()->evaluate());
+
+
+}
+
+
+
+
+
+
